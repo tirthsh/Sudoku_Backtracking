740f009706f4aab327ad2395921634b341d08701
Tirth Shah <tirth96@hotmail.com>
2019-12-18 01:38:44 -0500

    added two methods, building up to backtracking
    One method finds the next value to fill and another method make sures the board still follows the rules of Sudoku once a value is filled

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,3 +1,38 @@
+#this method valdiates the board, in other words whether the new number filled in follows the rules of sudoku
+# 3 cases
+# 1. no same value on the same row
+# 2. no same value on the same col
+# 3. no same value in the same board
+def validateBoard(board, num, pos):
+    #check row
+    for row in range(len(board[0])):
+        if board[pos[0]][row] == num and pos[1] != row:
+            return False
+
+    #check col
+    for col in range(len(board)):
+        if board[col][pos[1]] == num and pos[0] != col:
+            return False
+
+    #check square
+    boxX = pos[1] // 3
+    boxY = pos[0] // 3
+
+    for row in range(boxY * 3, boxY * 3 + 3):
+        for col in range(boxX * 3, boxX * 3 + 3):
+            if board[row][col] == num and (row, col) != pos:
+                return False
+
+#this method finds the next empty space, in other words the next 0 which needs to be filled in
+#returns a tuple of (row, col) coordinate of the 0 found, returns None if no 0s found (i.e. board is filled)
+def findNextEmptySpace(board):
+    for row in range (len(board)):
+        for col in range (len(board[0])):
+            if board[row][col] == 0:
+                return (row, col)
+
+    return None
+
 def printBoard(board):
     for row in range(len(board)):
         if row % 3 == 0 and row != 0:
@@ -20,7 +55,7 @@ def printBoard(board):
 if __name__ == "__main__":
     #sample board
     #0 represents blank space in which the solver will fill by backtracking
-    board = 
+    board = \
     [
       [7,8,0,4,0,0,1,2,0],
       [6,0,0,0,7,5,0,0,9],
